b5df1980ace07c0833cc720055d9a4aaad3df192
Tanishq Wadhwani <tanishqwadhwanitanu@gmail.com>
2025-11-13 15:49:53 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]:
+    ''' 
+    This function will return a List of requirements
+
+    '''
+
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        
+    
+    return requirements
+
+
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Tanishq',
+    author_email='tanishqwadhwanitanu@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+
+
+)
